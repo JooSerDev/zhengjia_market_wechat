40f0083a348156fb6141a8add35747bd0c399a88
JooSevDev <p@joosure.com>
2016-09-24 00:07:24 +0800

    1. DictsCache 的初始化方法增加到listener中

--- src/main/java/com/joosure/server/mvc/wechat/listener/InitialListener.java
+++ src/main/java/com/joosure/server/mvc/wechat/listener/InitialListener.java
@@ -1,5 +1,6 @@
 package com.joosure.server.mvc.wechat.listener;
 
+import com.joosure.server.mvc.wechat.dao.cache.DictsCache;
 import com.joosure.server.mvc.wechat.dao.cache.ItemCache;
 import com.shawn.server.core.listener.PreContextInitListener;
 
@@ -8,6 +9,7 @@ public class InitialListener extends PreContextInitListener {
 	@Override
 	public void Initial() {
 		ItemCache.inti();
+		DictsCache.initDicts();
 	}
 
 	@Override
